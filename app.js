f04efabf9a89ce04760ff4354e2d6eedf6cd90a8
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-05 12:24:36 -0700

    got renderCountry to display to the user

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // import functions and grab DOM elements
-// import { countries } from './countries.js';
-// import { renderCountry } from './utils.js';
+import { countries } from './countries.js';
+import { renderCountry } from './utils.js';
 
 // let state
+const countryListEl = document.getElementById('countries');
+
+for (let country of countries) {
+    const countryDiv = renderCountry(country);
+    countryListEl.append(countryDiv);
+}
+
 
 // set event listeners 
   // get user input
