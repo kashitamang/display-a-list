4886d8641df14dcfe97a6fdaac2a41bdf4b1c708
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-06 15:37:33 -0700

    ci check edits

--- signs.js
+++ signs.js
@@ -3,7 +3,7 @@ export const signs = [
         name: 'sagittarius',
         date: '(nov.21 - dec.21)',
         image: './assets/sagittarius.jpg',
-        saying: 'seeking, seeking, seeking.',
+        saying: 'seeking, seeking, seeking',
         type: 'sun',
         house: '9th',
         traits: ['higher education', 'foreign travel', 'spirituality', 'wisdom'],
