d45ef1d5378c113becc3f80df453c5e6228b2a5f
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-06 11:43:42 -0700

    added planets array objects

--- app.js
+++ app.js
@@ -2,15 +2,22 @@
 import { countries } from './countries.js';
 import { renderCountry } from './utils.js';
 
+import { planets } from './planets.js';
+import { renderPlanet } from './utils.js';
+
 // let state
 const countryListEl = document.getElementById('countries');
+const planetListEl = document.getElementById('planets');
 
 for (let country of countries) {
     const countryDiv = renderCountry(country);
     countryListEl.append(countryDiv);
 }
 
-
+for (let planet of planets) {
+    const planetDiv = renderPlanet(planet);
+    planetListEl.append(planetDiv);
+}
 // set event listeners 
   // get user input
   // use user input to update state 
--- utils.js
+++ utils.js
@@ -14,3 +14,30 @@ export function renderCountry(country) {
     div.append(h1, img, p);
     return div;
 }
+
+export function renderPlanet(planet) {
+    const div = document.createElement('div');
+    div.classList.add('planet');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = planet.name;
+
+    const img = document.createElement('img');
+    img.src = planet.image;
+
+    const p = document.createElement('p');
+    p.textContent = `${planet.name} is the ${planet.distance} planet from the sun`;
+
+    const h2 = document.createElement('h2');
+    h2.textContent = 'materials:';
+
+    const ul = document.createElement('ul');
+    for (let material of planet.materials) {
+        const li = document.createElement('li');
+        li.textContent = material;
+        ul.append(li);
+    }
+
+    div.append(h1, img, p, h2, ul);
+    return div;
+}
